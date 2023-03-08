c72a4a0a2387a3e05ee83ba3474757c002f79dba
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-08 15:15:21 +0000

    module 10 - lesson 5

--- src/playground/router-101.js
+++ src/playground/router-101.js
@@ -1,42 +1,59 @@
 const { createStore } = require("redux");
 
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const RESET = 'RESET';
+const actions = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  RESET: 'RESET',
+  SET: 'SET',
+}
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
-    case RESET:
+    case actions.INCREMENT:
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+      return { count: state.count + incrementBy };
+    case actions.DECREMENT:
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+      return { count: state.count - decrementBy };
+    case actions.RESET:
       return { count: 0 }
+    case actions.SET:
+      return { count: action.count || 0 }
     default:
       return state;
   };
 
 });
 
-console.log(store.getState().count);
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState().count);
+})
 store.dispatch({
-  type: INCREMENT
+  type: actions.INCREMENT,
+  incrementBy: 5
 });
-console.log(store.getState().count);
 store.dispatch({
-  type: INCREMENT
+  type: actions.INCREMENT
 });
-console.log(store.getState().count);
 store.dispatch({
-  type: INCREMENT
+  type: actions.INCREMENT
 });
-console.log(store.getState().count);
 store.dispatch({
-  type: DECREMENT
+  type: actions.RESET
 });
-console.log(store.getState().count);
 store.dispatch({
-  type: RESET
+  type: actions.DECREMENT
 });
+store.dispatch({
+  type: actions.DECREMENT,
+  decrementBy: 5
+});
+store.dispatch({
+  type: actions.SET,
+  count: 101
+})
+store.dispatch({
+  type: actions.SET,
+})
 
-console.log(store.getState());
+unsubscribe();
