cb93a2b5b19c8f3fcf165bf7c5bdfd57a6bd5b8f
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-10 11:33:38 +0000

    module 10 - lesson 11

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,19 +1,49 @@
 import { createStore, combineReducers } from "redux";
+import uuid from 'uuid';
+
+// ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0,
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt,
+  }
+});
+
+// REMOVE_EXPENSE
+const removeExpense = ({ id }) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
 
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id);
     default:
       return state;
   }
 };
 
 const filtersReducerDefaultState = {
-    text: '',
-    sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
-  };
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     default:
@@ -27,22 +57,19 @@ const store = createStore(
     filters: filtersReducer
   })
 );
-console.log(store.getState());
 
-const demoState = {
-  expenses: [{
-    id: 1,
-    description: 'food',
-    note: 'McDonalds',
-    amount: 500, // cents
-    createdAt: new Date()
-  }],
-  filters: {
-    text: '',
-    sortBy: 'amount',
-    startDate: undefined,
-    endDate: undefined
-  }
-};
+const unsub = store.subscribe(() =>
+  console.log(store.getState())
+);
+const ids = [];
+ids.push(
+  store.dispatch(addExpense({
+    description: 'Big Burgi',
+    amount: 500
+  })).expense.id);
+ids.push(store.dispatch(addExpense({
+  description: 'Small Burgi',
+  amount: 400
+})).expense.id);
 
-console.log();
+store.dispatch(removeExpense({id: ids[0]}));
