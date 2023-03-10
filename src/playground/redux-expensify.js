2bc26e1cea6f2d55a6949b7dc00a78a580cc12b7
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-10 14:12:06 +0000

    module 10 - lesson 13

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -39,6 +39,28 @@ const setTextFilter = (text = '') => ({
   text
 });
 
+// SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+});
+
+// SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE',
+});
+
+// SET_START_DATE
+const setStartDate = (date) => ({
+  type: 'SET_START_DATE',
+  date
+});
+
+// SET_END_DATE
+const setEndDate = (date) => ({
+  type: 'SET_END_DATE',
+  date
+});
+
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
@@ -52,6 +74,16 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
           ...expense,
           ...action.expense
         } : expense);
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount' 
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date' 
+      }
     default:
       return state;
   }
@@ -70,6 +102,16 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
       return {
         ...state,
         text: action.text,
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.date,
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.date,
       };
     default:
       return state;
@@ -97,6 +139,11 @@ ids.push(store.dispatch(addExpense({
   amount: 400
 })).expense.id);
 
-store.dispatch(removeExpense({ id: ids[0] }));
-store.dispatch(editExpense(ids[1], { amount: 600 }));
-store.dispatch(setTextFilter('Burgi'));
+// store.dispatch(removeExpense({ id: ids[0] }));
+// store.dispatch(editExpense(ids[1], { amount: 600 }));
+// store.dispatch(setTextFilter('Burgi'));
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(700));
