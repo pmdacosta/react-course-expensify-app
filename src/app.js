b115029bdc770da6d3b4107009ab5254f59e1963
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-13 15:28:13 +0000

    module 11 - lesson 7

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const expenses = [
   }, {
     description: 'Gas oven',
     note: 'May',
-    amount: 2000,
+    amount: 100,
     createdAt: 9000
   }, {
     description: 'Eletric bill',
@@ -33,8 +33,6 @@ const expenses = [
     createdAt: 3000
   }
 ].map(expense => store.dispatch(addExpense(expense)));
-
-store.dispatch(setTextFilter(''));
 
 const jsx = (
   <Provider store={store}>
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortByAmount, sortByDate } from "../actions/filters";
 
 const ExpenseListFilters = (props) => (
-  <input
-    type="text"
-    value={props.filters.text}
-    onChange={(e) => props.dispatch(setTextFilter(e.target.value))}
-  />
+  <div>
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => props.dispatch(setTextFilter(e.target.value))}
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        const sortByFunction = e.target.value === "amount" ? sortByAmount : sortByDate;
+        props.dispatch(sortByFunction());
+      }}>
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
+  </div>
 );
 
 const mapStateToProps = (state) => ({
